0f4c1ebba3ec31b1b73c9f44ecca574564d09446
PiepMatz <rappersinfan@gmail.com>
2024-02-08 10:13:51 +0100

    fix: CRON: Fixed an issue with service initialization

--- cron_restart.pl
+++ cron_restart.pl
@@ -8,37 +8,54 @@ $no_acl_check++;
 require "./paldmin-lib.pl";
 require "./rcon-lib.pl";
 
+init_service();
 init_rcon();
 
-# Check for rcon and if server is running
-if (is_server_running() && $rcon{"valid"} && $config{'scheduler_announce'} == 1) {
-	my ($ok, $exec, $output) = broadcast("Scheduled Restart in 15min...");
-	if (!$ok) {
-		# Announcement failed, do the restart
-		goto RESTART;
-	}
-	sleep(5 * 60);
-	($ok, $exec, $output) = broadcast("Scheduled Restart in 10min...");
-	if (!$ok) {
-		# Announcement failed, do the restart
-		goto RESTART;
-	}
-	sleep (5 * 60);
-	($ok, $exec, $output) = broadcast("Scheduled Restart in 5min...");
-	if (!$ok) {
-		# Announcement failed, do the restart
-		goto RESTART;
-	}
-	sleep (4 * 60);
-	($ok, $exec, $output) = broadcast("Scheduled Restart in 1min...");
-	if (!$ok) {
-		# Announcement failed, do the restart
-		goto RESTART;
-	}
-	sleep (60);
+if ($config{'scheduler_announce'} != 1) {
+	print STDOUT "Skipping announcement: Announcement not set.\n";
+	goto RESTART;
+}
+if (!is_server_running()) {
+	print STDOUT "Skipping announcement: Server not running.\n";
+	goto RESTART;
+}
+if (!$rcon{"valid"}) {
+	print STDOUT "Skipping announcement: RCON Setup is no valid:\n";
+	print STDOUT $rcon{"msg"}{"content"}."\n";
+	goto RESTART;
 }
 
+my ($ok, $exec, $output) = broadcast("Scheduled Restart in 15min...");
+if (!$ok) {
+	print STDOUT "Skipping announcement: Failed to make announcement #1.\n";
+	goto RESTART;
+}
+sleep(5 * 60);
+
+($ok, $exec, $output) = broadcast("Scheduled Restart in 10min...");
+if (!$ok) {
+	print STDOUT "Skipping announcement: Failed to make announcement #2.\n";
+	goto RESTART;
+}
+sleep(5 * 60);
+
+($ok, $exec, $output) = broadcast("Scheduled Restart in 5min...");
+if (!$ok) {
+	print STDOUT "Skipping announcement: Failed to make announcement #3.\n";
+	goto RESTART;
+}
+sleep(4 * 60);
+
+($ok, $exec, $output) = broadcast("Scheduled Restart in 1min...");
+if (!$ok) {
+	print STDOUT "Skipping announcement: Failed to make announcement #4.\n";
+	goto RESTART;
+}
+sleep(60);
+
+
 RESTART:
+print STDOUT "Restarting server...";
 restart_server();
 
 exit(0);
